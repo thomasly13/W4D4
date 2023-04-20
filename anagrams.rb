962a90293e4d5ce72b6488d4c0491612004a51fa
laurencary <armstronglg720@gmail.com>
2023-04-20 13:32:59 -0700

    anagrams draft

---
+++ anagrams.rb
@@ -0,0 +1,33 @@
+def permutations(word1)
+
+ 
+
+
+def first_anagram?(word1,word2)
+
+
+end
+
+"g"
+
+(0..1).each
+    "ig"
+    "gi"
+(0..2).each
+    "zig"
+    "izg"
+    "igz"
+    "zgi"
+    "gzi"
+    "giz"
+
+012
+120
+210
+021
+102
+201
+
+
+p anagram?("gizmo", "sally")    #=> false
+p anagram?("elvis", "lives")    #=> true
