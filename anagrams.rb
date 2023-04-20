ea55a8cd5fb742b738c1172a652c8b4dec8b038e
laurencary <armstronglg720@gmail.com>
2023-04-20 15:11:58 -0700

    first two anagrams problems

--- anagrams.rb
+++ anagrams.rb
@@ -1,33 +1,33 @@
-def permutations(word1)
-
- 
-
+require 'byebug'
 
 def first_anagram?(word1,word2)
+    permutations = word1.split("").permutation(word1.length).to_a.map(&:join)
+    permutations.each do |word|
+        if word2 == word
+            return true
+        end
+    end
 
-
+    false
 end
 
-"g"
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
 
-(0..1).each
-    "ig"
-    "gi"
-(0..2).each
-    "zig"
-    "izg"
-    "igz"
-    "zgi"
-    "gzi"
-    "giz"
+def second_anagram?(word1,word2)
+    split = word2.split("")
+    word1.each_char.with_index do |char, index|
+        if split.index(char) == nil
+            return false
+        else
+            split.delete_at(split.index(char))
+        end
+    end
+    if split.length == 0
+        return true
+    end
+    false
+end
 
-012
-120
-210
-021
-102
-201
-
-
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
