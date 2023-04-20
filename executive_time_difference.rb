7e8f7819b5d595d7afa3ec20d8e5b4a2c8e45476
laurencary <armstronglg720@gmail.com>
2023-04-20 12:09:10 -0700

    completed largest contiguous subsum problems

--- executive_time_difference.rb
+++ executive_time_difference.rb
@@ -34,3 +34,38 @@
 # p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
 # 2 + n(2) + 1 > n
 
+#phase I
+def largest_contiguous_subsum(list)
+    subs = []
+    list.each_with_index do |number, index|
+        (index...list.length).each do |range_index|
+            subs << list[index..range_index]
+        end
+    end
+    subs.map! {|arrays| arrays.sum}
+    subs.max
+end
+
+#phase II
+# def largest_contiguous_subsum(list)
+#     largest_sum = 0
+#     list.each_with_index do |number, index|
+#         (index...list.length).each do |range_index|
+#             sub_num = list[index..range_index]
+                   
+#             if largest_sum == 0
+#                 largest_sum = sub_num.sum
+#             elsif largest_sum < sub_num.sum
+#                 largest_sum = sub_num.sum
+#             end
+#         end
+#     end
+#     largest_sum
+# end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list)
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list)
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list)
