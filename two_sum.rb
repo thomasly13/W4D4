fe45653eeadb15dbdc6ebf8a9b8140ff19ae8bf9
laurencary <armstronglg720@gmail.com>
2023-04-20 16:37:49 -0700

    finished two sum

--- two_sum.rb
+++ two_sum.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 # def bad_two_sum?(arr, target_sum)
 
 #     (0..arr.length - 2).each do |i|
@@ -10,14 +10,14 @@
 # end
 
 def okay_two_sum?(arr, target_sum)
-    sorted = arr.sort 
+    sorted = arr.sort
     first = 0
-    last = arr.length - 1
+    last = sorted.length - 1
 
     until first == last
-        if arr[first] + arr[last] < target_sum
+        if sorted[first] + sorted[last] < target_sum
             first += 1
-        elsif arr[first] + arr[last] > target_sum
+        elsif sorted[first] + sorted[last] > target_sum
             last -= 1 
         else
             return true
@@ -27,11 +27,31 @@ def okay_two_sum?(arr, target_sum)
 false
 end
 
+arr = [0, 7, 5, 1] # 0, 1, 5, 7
+# p okay_two_sum?(arr, 6) # => should be true
+# 0 + 7 , 0 + 5, 1 + 7
+# p okay_two_sum?(arr, 10) # => should be false
+
+def two_sum?(arr, target_sum)
+    hash = {}
+
+    arr.each do |num|
+        # debugger
+        difference = target_sum - num
+        if difference == num
+            next
+        end
+        
+        if hash[num]
+            return true
+        else
+            hash[difference] = true
+        end
+    end
+    false
+end
 
 arr = [0, 7, 5, 1] # 0, 1, 5, 7
-p okay_two_sum?(arr, 6) # => should be true
-# 0 + 7 , 0 + 5, 1 + 7
-p okay_two_sum?(arr, 10) # => should be false
+p two_sum?(arr, 6) # => should be true
+p two_sum?(arr, 10) # => should be false
 
-# def two_sum?
-# end
