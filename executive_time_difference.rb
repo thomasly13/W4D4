89396f55ed1e73d5ab65f824e80f49a8b27e493a
Thomas Ly <Thomaslyy13@gmail.com>
2023-04-20 11:53:02 -0700

    my_min exercises

---
+++ executive_time_difference.rb
@@ -0,0 +1,36 @@
+
+# def my_min(list)
+
+#     list.each do |items_1|
+#         switch = true
+#         list.each do |items_2|
+#             if items_2 < items_1
+#                 switch = false
+#             end
+#         end
+#         return items_1 if switch == true
+#     end
+
+# end
+
+# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+# n(1 + n(1 + 1) + 2) > n(3 +n(2)) > 3n + 2n^2
+
+
+
+# def my_min(list)
+
+#     min = list.pop
+
+#     list.each do |items|
+#         if items < min
+#             min = items
+#         end
+#     end
+    
+# return min
+# end
+
+# p my_min([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+# 2 + n(2) + 1 > n
+
